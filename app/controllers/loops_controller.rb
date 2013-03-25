a74e4a78968f689a5348e9c69fc20a857030b78d
Damien Del Russo <ddelruss@mac.com>
2013-03-24 22:06:14 -0700

    html not getting node substitution. Need to add google lookup next.

--- app/controllers/loops_controller.rb
+++ app/controllers/loops_controller.rb
@@ -13,11 +13,15 @@ class LoopsController < ApplicationController
     
     flash[:message2] = "The params are: " + aString
     @current_login = params[:login]
+
+    process_google_doc(params[@current_login])
+    flash[:message1] = "Node dataset: " + @node_dataset
     
     if params[:commit].eql?("Connections")
-      redirect_to :action => "connections"
+      render :action => "connections"
     end
   end
+  
   def connections
     aString = "Connections params: "
     params.each do |p| 
@@ -27,4 +31,16 @@ class LoopsController < ApplicationController
     flash[:message2] = "Connections params are: " + aString
     @current_login = params[:login]  
   end
+  
+  def process_google_doc(login_hash)
+    @nodes = [["Person", "Damien", "A note", "some tags"],["Person", "Grant", "", ""],["Person", "Kelly", "", ""]]
+    @links = []
+    @node_dataset = "[";
+    @nodes.each do |node|
+      @node_dataset = @node_dataset + node.to_s + ","
+    end
+    @node_dataset.chomp!(',')
+    @node_dataset += "]"
+  end
+  
 end
