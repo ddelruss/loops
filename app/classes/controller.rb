37ec273d3eb8586311ce5cae257ef5ab96ec6218
Damien Del Russo <ddelruss@mac.com>
2013-04-03 20:51:23 -0700

    Refactored google doc and incorporated changes into controller

--- app/classes/controller.rb
+++ app/classes/controller.rb
@@ -1,32 +1,43 @@
 module Loops
   class Controller
     def initialize(document)
-      @client_name = "Client"
-      @thoughtworks_name = "ThoughtWorks"
+      @client_name = document.worksheet(0).title
+      @company_name = document.worksheet(1).title
       @nodes = []
       @links = []
-      ws = document.worksheet(0)
-
+      @reports_to = "Reports To"
+      
       #seed data with two organizations
-      @nodes = @nodes + [Node.new("Organization,#{@client_name}"), Node.new("ThoughtWorks,#{@thoughtworks_name}")]
+      @nodes = @nodes + [Node.new("Organization,#{@client_name}"), Node.new("ThoughtWorks,#{@company_name}")]
       
       # these are proprietary, that is, you have to know the format
-      # first worksheet is clients with format Name, Title, Notes, View of Thoughtworks
-      for row in 2..ws.num_rows
+      # first worksheet is clients with format Name, Title, View of Thoughtworks, Reports To, and Notes
+      # row 1 is the heading
+      ws = document.worksheet(0)
+      for row in 2..ws.num_rows  
         # p ws[row, 1]
-        @nodes = @nodes + [Node.new("Client Person,#{ws[row, 1]},#{ws[row, 2]},#{ws[row, 3]},#{ws[row, 4]}")]
+        # node expects node type, name, title, notes, view
+        @nodes = @nodes + [Node.new("Client Person,#{ws[row, 1]},#{ws[row, 2]},#{ws[row, 5]},#{ws[row, 3]}")]
         @links = @links + [Link.new("#{@client_name}, #{ws[row, 1]},Employee")]
       end      
-      # second worksheet is TWers with format Name (that's it)
+      
+      # second worksheet is source organization with format Name (that's it)
       ws = document.worksheet(1)
       for row in 2..ws.num_rows
-        @nodes = @nodes + [Node.new("TW Person,#{ws[row, 1]},#{ws[row, 2]}")]
-        @links = @links + [Link.new("#{@thoughtworks_name}, #{ws[row, 1]},Employee")]
+        @nodes = @nodes + [Node.new("Company Person,#{ws[row, 1]},#{ws[row, 2]}")]
+        @links = @links + [Link.new("#{@company_name}, #{ws[row, 1]},Employee")]
       end      
       # third worksheet is links from anyone to anyone
       ws = document.worksheet(2)
       for row in 2..ws.num_rows
         @links = @links + [Link.new("#{ws[row, 1]}, #{ws[row, 2]},#{ws[row, 3]}")]
+      end      
+      # back to the first worksheet for hierarchy
+      ws = document.worksheet(0)
+      for row in 2..ws.num_rows
+        if ws[row,4].length > 0 && !(ws[row, 1] == ws[row, 4]) 
+          @links = @links + [Link.new("#{ws[row, 1]}, #{ws[row, 4]},#{@reports_to}")]
+        end
       end      
       
     end
@@ -37,37 +48,6 @@ module Loops
     def link_count()
       @links.length
     end
-    # def write_csv(filename)
-    #   file = File.new(filename, "w")
-    #   file.write(Node.csv_title + "\n")
-    #   @nodes.each {|node|
-    #     file.write(node.csv_data + "\n")
-    #   }
-    # end
-    # def write_d3_node_summary(destination_filename, template_filename, d3_select_filename) 
-    #   template = File.new(template_filename, "r")
-    #   file = File.new(destination_filename, "w")
-    #   while (line = template.gets)
-    #     if (line.eql?("#custom_code_here\n"))
-    #       file.write("var dataset = [];\n")
-    #       @nodes.each {|node|
-    #         file.write("dataset.push(#{node.as_array.inspect});\n")            
-    #       }
-    #       write_file_in_file(d3_select_filename, file)
-    #     else
-    #       file.write(line)
-    #     end
-    #   end
-    #   template.close
-    #   file.close
-    # end
-    # def write_file_in_file(input, output)
-    #   file = File.new(input, "r")
-    #   while (line = file.gets)
-    #     output.write(line)
-    #   end
-    #   file.close
-    # end
     def d3_graph_json
       output = "{\"nodes\":["
       @nodes.each {|node|
@@ -109,7 +89,7 @@ module Loops
         end
       }
       output = output.chop + "],\"links\":["
-      output = write_json_links(output, filtered_nodes, "Reports")
+      output = write_json_links(output, filtered_nodes, @reports_to)
       output = output.chop + "]}"
     end
 
@@ -127,6 +107,69 @@ module Loops
       }
       output
     end
+    
+    def node_with_description(nodes, description)
+      nodes.each {|node|
+        if node.description.eql?(description) 
+          return node
+        end
+      }
+      nil
+    end
+    def reports_to_count(description)
+      @visited_nodes = []
+      count = 0
+      @links.each {|link|
+        if link.target == description && link.label == @reports_to && !@visited_nodes.index(link.source)
+          count += 1 + reports_to_count(link.source)
+          @visited_nodes = @visited_nodes + [link.source]
+        end
+      }
+      count
+    end
+    def connections_count(description)
+      count = 0
+      @links.each {|link|
+        if link.target == description || link.source == description
+          count += 1
+        end
+      }
+      count
+      
+    end
+    
+    
+    # def write_csv(filename)
+    #   file = File.new(filename, "w")
+    #   file.write(Node.csv_title + "\n")
+    #   @nodes.each {|node|
+    #     file.write(node.csv_data + "\n")
+    #   }
+    # end
+    # def write_d3_node_summary(destination_filename, template_filename, d3_select_filename) 
+    #   template = File.new(template_filename, "r")
+    #   file = File.new(destination_filename, "w")
+    #   while (line = template.gets)
+    #     if (line.eql?("#custom_code_here\n"))
+    #       file.write("var dataset = [];\n")
+    #       @nodes.each {|node|
+    #         file.write("dataset.push(#{node.as_array.inspect});\n")            
+    #       }
+    #       write_file_in_file(d3_select_filename, file)
+    #     else
+    #       file.write(line)
+    #     end
+    #   end
+    #   template.close
+    #   file.close
+    # end
+    # def write_file_in_file(input, output)
+    #   file = File.new(input, "r")
+    #   while (line = file.gets)
+    #     output.write(line)
+    #   end
+    #   file.close
+    # end
     
     # def write_d3_graph_json(filename)
     #   file = File.new(filename, "w")
@@ -164,35 +207,6 @@ module Loops
     #   output = output.chop + "]}"
     #   file.write(output)            
     # end
-    def node_with_description(nodes, description)
-      nodes.each {|node|
-        if node.description.eql?(description) 
-          return node
-        end
-      }
-      nil
-    end
-    def reports_to_count(description)
-      @visited_nodes = []
-      count = 0
-      @links.each {|link|
-        if link.target == description && link.label == "Reports To" && !@visited_nodes.index(link.source)
-          count += 1 + reports_to_count(link.source)
-          @visited_nodes = @visited_nodes + [link.source]
-        end
-      }
-      count
-    end
-    def connections_count(description)
-      count = 0
-      @links.each {|link|
-        if link.target == description || link.source == description
-          count += 1
-        end
-      }
-      count
-      
-    end
   end
 end
 
