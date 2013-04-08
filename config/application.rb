7321713671ce67d13aa3dfb60174b1ec45915828
Damien Del Russo <ddelruss@mac.com>
2013-04-07 17:49:09 -0700

    more Heroku setup

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ end
 
 module Loops
   class Application < Rails::Application
+    
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile=false
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
